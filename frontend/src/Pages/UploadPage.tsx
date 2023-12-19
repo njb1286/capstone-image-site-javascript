5f37a65b3ae9bf22284adc05dd60f065a4085239
Njb1286 <bennionnj@gmail.com>
2023-12-19 15:29:13 -0700

    Installed React Bootstrap, added an image uploader element

--- frontend/src/Pages/UploadPage.tsx
+++ frontend/src/Pages/UploadPage.tsx
@@ -1,5 +1,6 @@
 import { FormEvent } from "react";
 import classes from "./UploadPage.module.scss";
+import { FormControl, FormGroup } from "react-bootstrap";
 
 function UploadPage() {
   const submitHandler = (event: FormEvent<HTMLFormElement>) => {
@@ -7,10 +8,15 @@ function UploadPage() {
   }
 
   return <form className={classes.form} onSubmit={submitHandler}>
-    <div className="form-group">
+    <FormGroup>
       <label htmlFor="image-title">Title</label>
-      <input placeholder="Enter title..." type="text" className="form-control" id="image-title" />
-    </div>
+      <FormControl placeholder="Enter title..." id="image-title"></FormControl>
+    </FormGroup>
+
+    <FormGroup>
+      <label htmlFor="image-file-uploader">Image</label>
+      <FormControl type="file" id="image-file-uploader" accept=".png .jpg .jpeg" />
+    </FormGroup>
   </form>;
 }
 

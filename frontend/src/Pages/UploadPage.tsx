633c561d84276eece4240df645dbb6622230dcd7
Njb1286 <bennionnj@gmail.com>
2023-12-19 15:15:11 -0700

    Changed the title of the site to match

--- frontend/src/Pages/UploadPage.tsx
+++ frontend/src/Pages/UploadPage.tsx
@@ -1,5 +1,17 @@
+import { FormEvent } from "react";
+import classes from "./UploadPage.module.scss";
+
 function UploadPage() {
-  return <></>;
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
+  return <form className={classes.form} onSubmit={submitHandler}>
+    <div className="form-group">
+      <label htmlFor="image-title">Title</label>
+      <input placeholder="Enter title..." type="text" className="form-control" id="image-title" />
+    </div>
+  </form>;
 }
 
 export default UploadPage;

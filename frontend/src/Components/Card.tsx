1894b78413a29ce43961b73971f93537c7f255d9
Njb1286 <bennionnj@gmail.com>
2024-01-15 17:07:06 -0700

    Update card width and grid template columns

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -6,14 +6,12 @@ import { backendUrl } from "../store/backend-url";
 function Card({ title, description, id }: Readonly<ImageItem>) {
   return (
     <NavLink to={`/views?id=${id}`} className={`card text-center ${classes.card}`}>
-      <h3 className="card-header">{title}</h3>
-      <div className={classes["body-content"]}>
-        <div className={classes["image-wrapper"]}>
-          <img alt={title} src={`${backendUrl}/get-image?id=${id}`} className={`card-img ${classes.image}`} />
-        </div>
-        <div className="card-body">
-          <p className="card-text text-start">{description.substring(0, 200)}{description.length > 200 ? "..." : ""}</p>
-        </div>
+      <div className={`card-header ${classes.header}`}>
+        <h2>{title}</h2>
+      </div>
+      <img alt={title} src={`${backendUrl}/get-image?id=${id}`} className={`card-img ${classes.image}`} />
+      <div className="card-body">
+        <p className="card-text text-start">{description.substring(0, 200)}{description.length > 200 ? "..." : ""}</p>
       </div>
     </NavLink>
   );

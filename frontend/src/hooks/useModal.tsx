c2b3d781206f14da1490284caa72bcc2e260ce84
Njb1286 <bennionnj@gmail.com>
2024-01-16 21:48:53 -0700

    Refactor useModal hook to use Redux for managing modal state

--- frontend/src/hooks/useModal.tsx
+++ frontend/src/hooks/useModal.tsx
@@ -1,7 +1,24 @@
-import { useState } from "react";
+import { Dispatch } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { ModalAction } from "../store/modal/modal-store";
 
-export const useModal = () => {
-  const [visible, setVisible] = useState(false);
+export const useModal = (title: string, content: string) => {
+  const dispatch = useDispatch<Dispatch<ModalAction>>();
+
+  const setVisible = (payload: boolean) => {
+    dispatch({
+      type: "SET_MODAL_DATA",
+      payload: {
+        title,
+        content
+      }
+    })
+
+    dispatch({
+      type: "SET_MODAL_VISIBLE",
+      payload,
+    });
+  }
 
   return [setVisible] as const;
 }

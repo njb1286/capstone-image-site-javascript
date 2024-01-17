899649863cc3a75ca865e27cc556eaa597f524c4
Njb1286 <bennionnj@gmail.com>
2024-01-17 13:50:45 -0700

    Refactor Card component layout to not include the description

--- frontend/src/Components/Card.tsx
+++ frontend/src/Components/Card.tsx
@@ -3,15 +3,14 @@ import classes from "./Card.module.scss";
 import { ImageItem } from "../store/images-store";
 import { backendUrl } from "../store/backend-url";
 
-function Card({ title, description, id }: Readonly<ImageItem>) {
+function Card({ title, id }: Readonly<ImageItem>) {
   return (
     <NavLink to={`/views?id=${id}`} className={`card text-center ${classes.card}`}>
       <div className={`card-header ${classes.header}`}>
         <h2>{title}</h2>
       </div>
+      <div className={classes["image-wrapper"]}>
       <img alt={title} src={`${backendUrl}/get-image?id=${id}`} className={`card-img ${classes.image}`} />
-      <div className="card-body">
-        <p className="card-text text-start">{description.substring(0, 200)}{description.length > 200 ? "..." : ""}</p>
       </div>
     </NavLink>
   );

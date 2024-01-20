0245280275ae8966784bd183395a644061f95348
Njb1286 <bennionnj@gmail.com>
2024-01-19 19:21:27 -0700

    Added a spinner to the searchbar, and made it only search after 250 ms of not typing

--- frontend/src/Components/SearchBar.tsx
+++ frontend/src/Components/SearchBar.tsx
@@ -1,29 +1,42 @@
 import { useDispatch } from "react-redux";
 import classes from "./SearchBar.module.scss";
 import { ImageActions, categories } from "../store/images-store";
-import { InputGroup } from "react-bootstrap";
+import { InputGroup, Spinner } from "react-bootstrap";
 import CategoriesDropdown from "./CategoriesDropdown";
 import { Dispatch } from "@reduxjs/toolkit";
+import { useEffect, useState } from "react";
 
 const searchBarCategories = ["All", ...categories] as const;
 export type SearchBarCategory = typeof searchBarCategories[number];
 
 function SearchBar() {
   const dispatch = useDispatch<Dispatch<ImageActions>>();
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
 
+  useEffect(() => {
+    setIsSearching(true);
 
-  const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.preventDefault();
+    const timeout = setTimeout(() => {
+      dispatch({
+        type: "SET_SEARCH_VALUE",
+        payload: searchValue,
+      });
 
-    const payload = event.target.value;
+      setIsSearching(false);
+    }, 250);
 
-    dispatch({
-      type: "SET_SEARCH_VALUE",
-      payload,
-    })
+    return () => {
+      clearTimeout(timeout);
+      setIsSearching(false);
+    }
+  }, [searchValue]);
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
   }
 
-  const selectHandler = (category: SearchBarCategory) => {    
+  const selectHandler = (category: SearchBarCategory) => {
     dispatch({
       type: "SET_SELECTED_CATEGORY",
       payload: category,
@@ -33,9 +46,10 @@ function SearchBar() {
   return (
     <div className={classes["search-bar-wrapper"]}>
       <InputGroup className={classes["search-section"]}>
-      <input onChange={inputHandler} type="text" className={`form-control ${classes["search-input"]}`} placeholder="Search..." />
-      <CategoriesDropdown title="Filter" onSelect={selectHandler} categories={searchBarCategories} default="All" />
-    </InputGroup>
+        <input onChange={changeHandler} type="text" className={`form-control ${classes["search-input"]}`} placeholder="Search..." />
+        <CategoriesDropdown title="Filter" onSelect={selectHandler} categories={searchBarCategories} default="All" />
+      </InputGroup>
+      {isSearching && <div className={classes["spinner-wrapper"]}><Spinner animation="border" variant="primary" /></div>}
     </div>
   )
 }

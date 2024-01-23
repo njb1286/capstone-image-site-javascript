7c9cf14c0abcec47400b2f7dc2351330efc69155
Njb1286 <bennionnj@gmail.com>
2024-01-22 19:24:45 -0700

    Refactor GeneratePasswordPage component

--- frontend/src/Pages/GeneratePasswordPage.tsx
+++ frontend/src/Pages/GeneratePasswordPage.tsx
@@ -1,13 +1,15 @@
-import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+
+import classes from "./GeneratePasswordPage.module.scss";
 
 const GeneratePasswordPage = () => {
   return (
-    <Form>
-      <Form.Group controlId="formField">
-        <Form.Label>Enter a value:</Form.Label>
-        <Form.Control type="text" placeholder="Enter a value" />
-      </Form.Group>
-    </Form>
+    <div className={`d-flex fs-4 justify-content-start align-items-center flex-column vh-100 ${classes.container}`}>
+      <h1 className={classes.title}>Generate a single use password</h1>
+      <Button variant="primary" size="lg" className={classes.button}>
+        Generate
+      </Button>
+    </div>
   );
 };
 
